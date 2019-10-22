9994005783c994c2d6cdbd84020e870abd364aac
Dmitry G. Anderson <dandgerson@gmail.com>
2019-10-22 23:22:50 +0300

    Transition & Transition with children & CSSTransition

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import './App.scss';
+
+import TransitionComponent from './components/TransitionComponent'
+import TransitionWithChildren from './components/TransitionWithChildren'
+import CSSTransitionComponent from './components/CSSTransitionComponent'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1>
+          Training React Transition Group Components.
+        </h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://reactcommunity.org/react-transition-group/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Transition Group
         </a>
       </header>
+      <main className="App-content">
+        <TransitionComponent />
+        <TransitionWithChildren />
+        <CSSTransitionComponent />
+        {[...Array(2)].map((_, i) => (
+          <div key={i} className="App-item">
+            <span>
+              Deserunt enim nisi in fugiat sunt consectetur nostrud eu ad consectetur officia.
+            </span>
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
