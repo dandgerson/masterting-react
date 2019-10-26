3f3a5494d752bdf65bf7b0989c8feff39f16e617
Dmitry G. Anderson <dandgerson@gmail.com>
2019-10-26 11:41:34 +0300

    switch to ref

--- src/components/HelloReact.js
+++ src/components/HelloReact.js
@@ -3,16 +3,18 @@ import React, { useState } from 'react'
 import HelloMessage from './HelloMessage'
 
 const HelloReact = () => {
-  const [ state, setState ] = useState({ message: 'I am from default state' })
-
-  const updateMessage = e => {
-    setState({ message: e.target.value })
+  const [ state, setState ] = useState({ message: 'default' })
+  let $input = null
+  const updateMessage = () => {
+    console.log('updateMessage')
+    setState({ message: $input.value })
   }
   return (
     <div>
       <input
         type="text"
         onChange={updateMessage}
+        ref={elem => ($input = elem)}
         />
       <h1>Hello React!</h1>
       <HelloMessage message={state.message} />
