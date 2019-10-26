7909a898f434b5a5790584a9c12ebb00182dd859
Dmitry G. Anderson <dandgerson@gmail.com>
2019-10-26 10:24:25 +0300

    use props as local variable

--- src/components/HelloMessage.js
+++ src/components/HelloMessage.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
-const HelloMessage = ({ message }) => (
-  <div>
-    <h2>Hello Message:</h2>
-    <div>{message}</div>
-  </div>
-)
+const HelloMessage = ({ message }) => {
+  const localprops = message
+  const localMessage = 'This is the local message!'
+  return (
+    <div>
+      <h2>Hello Message:</h2>
+      <div>{message}</div>
+      <div>{localprops}</div>
+      <div>{localMessage}</div>
+    </div>
+  )
+}
 
 export default HelloMessage
