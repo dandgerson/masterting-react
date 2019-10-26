784dd49482608146990124c82ad413b8457ebcdf
Dmitry G. Anderson <dandgerson@gmail.com>
2019-10-26 10:48:33 +0300

    add default props

--- src/components/HelloMessage.js
+++ src/components/HelloMessage.js
@@ -1,21 +1,36 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const HelloMessage = ({ message }) => {
+const HelloMessage = ({ message, ...props}) => {
+  console.log(props)
   const localprops = message
   const localMessage = 'This is the local message!'
   return (
     <div>
       <h2>Hello Message:</h2>
-      <div>{message}</div>
-      <div>{localprops}</div>
-      <div>{localMessage}</div>
+      <div style={{color: 'greenyellow'}}>
+        {message}
+      </div>
+      <div style={{color: 'limegreen'}}>
+        {props.defaultMessage}
+      </div>
+      <div style={{color: 'mediumspringgreen'}}>
+        {localprops}
+      </div>
+      <div style={{color: 'mediumseagreen'}}>
+        {localMessage}
+      </div>
     </div>
   )
 }
 
 export default HelloMessage
 
+HelloMessage.defaultProps = {
+  defaultMessage: 'I\'m default message. Hello! =)'
+}
+
 HelloMessage.propTypes = {
   message: PropTypes.string.isRequired,
+  defaultMessage: PropTypes.string,
 }
