b2392a2db677fac6532d28c4e46e064d045b4abe
Dmitry G. Anderson <dandgerson@gmail.com>
2019-11-11 19:12:06 +0300

    refactor TextBoxReducer

--- src/redux/reducers/HelloReactReducer.js
+++ src/redux/reducers/HelloReactReducer.js
@@ -1,32 +1,15 @@
 import {
-  SET_EDITING,
   SET_FIRSTNAME,
   SET_LASTNAME,
   SET_SHOW_MESSAGE,
 } from '../constants/action-types'
 
 export default (state = {
-  textBoxsEditStatus: [
-    {
-      id: 0,
-      editing: false,
-    },
-    {
-      id: 1,
-      editing: false,
-    },
-  ],
   firstName: '',
   lastName: '',
   showMessage: true,
 }, action) => {
   switch (action.type) {
-    case SET_EDITING: {
-      return {
-        ...state,
-        ...action.payload,
-      }
-    }
     case SET_FIRSTNAME: {
       return {
         ...state,
--- src/redux/reducers/UserListReducer.js
+++ src/redux/reducers/UserListReducer.js
@@ -2,27 +2,26 @@ import {
   SET_USERS,
 } from '../constants/action-types'
 
-export default (state = {
-  users: [
-    {
-      id: 1,
-      userName: 'RiyanVice',
-      email: 'ryan@vicesoftware.com',
-    },
-    {
-      id: 2,
-      userName: 'AdamHorton',
-      email: 'digitalicarus@gmail.com',
-    },
-  ]
-}, action) => {
+export default (state = [
+  {
+    id: 1,
+    userName: 'RiyanVice',
+    email: 'ryan@vicesoftware.com',
+  },
+  {
+    id: 2,
+    userName: 'AdamHorton',
+    email: 'digitalicarus@gmail.com',
+  },
+],
+  action) => {
   switch (action.type) {
     case SET_USERS: {
       return [
         ...state,
         ...action.payload,
       ]
-      
+
     }
     default:
       return state
